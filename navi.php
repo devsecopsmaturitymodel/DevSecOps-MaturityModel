0204169676c9349bf43a9d23753daa8410113430
Timo Pagel <gitlab@timo-pagel.de>
2016-03-01 18:05:09 +0100

    MOD: Update

--- navi.php
+++ navi.php
@@ -1,4 +1,4 @@
 <?php
-echo '<a href="index.php">Tabelle und Diagramm</a><br />';
+echo '<a href="index.php">Tabelle</a><br />';
 echo '<a href="scutter.php">Nutzen und Implementierung-Diagramm</a><br />';
 echo '<a href="graph.php">Abhängigkeiten</a>';
--- scutter.php
+++ scutter.php
@@ -1,28 +1,6 @@
-<html>
-<head>
-    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
-          integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-            crossorigin="anonymous"></script>
-
-    <link href="http://nvd3.org/assets/css/common.css" rel="stylesheet">
-    <link href="http://nvd3.org/assets/css/nv.d3.css" rel="stylesheet">
-
-    <script src="http://nvd3.org/assets/js/lib/bootstrap.min.js"></script>
-    <script src="http://nvd3.org/assets/lib/d3.v3.js"></script>
-    <script src="http://nvd3.org/assets/js/nv.d3.js"></script>
-
-</head>
+<?php
+include_once "head.php";
+?>
 <body>
 <?php
 include_once "navi.php";
