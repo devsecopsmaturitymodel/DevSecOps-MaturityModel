d216e4aaba52b236abc361da5c088dcf6c67da44
Timo Pagel <gitlab@timo-pagel.de>
2016-03-01 18:05:57 +0100

    MOD: Add br

--- navi.php
+++ navi.php
@@ -1,4 +1,4 @@
 <?php
 echo '<a href="index.php">Tabelle</a><br />';
 echo '<a href="scutter.php">Nutzen und Implementierung-Diagramm</a><br />';
-echo '<a href="graph.php">Abhängigkeiten</a>';
+echo '<a href="graph.php">Abhängigkeiten</a><br />';
