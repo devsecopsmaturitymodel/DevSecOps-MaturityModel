7ac6bec8b6b1c8babeb42b9a23a7b99caf92492d
Roberto Polli <robipolli@gmail.com>
2021-02-10 12:45:22 +0100

    risk can be a list.

--- data.php
+++ data.php
@@ -172,6 +172,14 @@ function isAssoc(array $arr)
     return array_keys($arr) !== range(0, count($arr) - 1);
 }
 
+
+function render_risk($risk) {
+
+    if (is_array($risk)) {
+        return implode("\ ", $risk);
+    }
+    return $risk;
+}
 function build_table_tooltip($array, $headerWeight = 2)
 {
     $mapKnowLedge = array("Very Low (one discipline)", "Low (one discipline)", "Medium (two disciplines)", "High (two disciplines)", "Very High (three or more disciplines)");
@@ -187,7 +195,7 @@ function build_table_tooltip($array, $headerWeight = 2)
 
     $html = "";
     $html .= "<h" . $headerWeight . ">Risk and Opportunity</h$headerWeight>";
-    $html .= "<div><b>" . "Risk" . ":</b> " . $array['risk'] . "</div>";
+    $html .= "<div><b>" . "Risk" . ":</b> " . render_risk($array['risk']) . "</div>";
     $html .= "<div><b>" . "Opportunity" . ":</b> " . $array['measure'] . "</div>";
     if (IS_SHOW_EVIDENCE_TODO || $evidenceContent != "TODO")
         $html .= "<div><b>" . "Evidence" . ":</b> " . $evidenceContent . "</div>";
