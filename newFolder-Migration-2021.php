4b6eaacbaa75eeb1fecd3dbf6228972f9a4336a1
Timo Pagel <github@timo-pagel.de>
2021-02-11 19:04:36 +0100

    Use composer

--- newFolder-Migration-2021.php
+++ newFolder-Migration-2021.php
@@ -23,27 +23,28 @@ foreach ($dimensions as $dimensionName => $subDimension) {
         $newElements = $elements;
         ksort($newElements);
         $dimensions[$dimensionName][$subDimensionName] = $newElements;
+
         foreach($elements as $elementName => $element) {
-            $dimensionIdWithSpace = mb_convert_case($dimensionName, MB_CASE_TITLE);
-            $dimensionId = str_replace(" ", "", $dimensionIdWithSpace);
-            $subDimensionId = str_replace(" ", "",mb_convert_case($subDimensionName, MB_CASE_TITLE));
-            $folder = "data-new/$dimensionId/$subDimensionId/" . $element['level'] . "/";
-            //echo $folder;
-            mkdir($folder, 755, true);
-            unset($element['level']); // level from folder
+            //unset($element['level']); // level from folder
             $idWithSpace=mb_convert_case($elementName, MB_CASE_TITLE);
             $idSanitized =  str_replace("/", "Or", $idWithSpace);
 
             $id =  str_replace(" ", "", $idSanitized);
-            $filePath = $folder . $id . ".yaml";
             $element = array('id' => $id) + array('title' => $elementName) +  $element; // Add properties to the top
             $element['id'] = $id;
-            $yamlContent = yaml_emit ( $element );
-
-            file_put_contents($filePath, $yamlContent);
-
         }
+        $dimensionIdWithSpace = mb_convert_case($dimensionName, MB_CASE_TITLE);
+        $dimensionId = str_replace(" ", "", $dimensionIdWithSpace);
+        $subDimensionId = str_replace(" ", "",mb_convert_case($subDimensionName, MB_CASE_TITLE));
+        $folder = "data-new/$dimensionId/";
+        //echo $folder;
+        mkdir($folder, 755, true);
 
+        $filePath = $folder . $subDimensionId . ".yaml";
+        echo $filePath;
+        $yamlContent = yaml_emit(array($subDimensionName => $elements));
+
+        file_put_contents($filePath, $yamlContent);
     }
 }
 
--- startDocker.bash
+++ startDocker.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t wurstbrot/dsomm:latest .
 docker rm -f dsomm || true
-docker run --rm -p 81:80 -v "$PWD":/var/www/html --name dsomm wurstbrot/dsomm
 
+docker run --rm -p 81:80 --name dsomm wurstbrot/dsomm
+#docker run --rm -p 81:80 -v "$PWD":/var/www/html --name dsomm wurstbrot/dsomm
