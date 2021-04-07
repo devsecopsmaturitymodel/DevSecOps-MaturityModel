ebeff09a6257d566389fb0dba77d52ef64e64a67
Roberto Polli <roberto.polli@par-tec.it>
2021-04-07 10:13:43 +0200

    improve tests, reformat file.

---
+++ tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+test -f phptidy.php || wget https://raw.githubusercontent.com/cmrcx/phptidy/master/phptidy.php
+
+echo "
+Reformat code according to .phptidy-config.php
+in order do minimize patch differences.
+"
+php ./phptidy.php replace *.php
+
+echo "
+Checking php syntax.
+"
+find  > /tmp/php-check.out 2>&1 . -type f -name \*.php -exec php -l {} \;
+cat /tmp/php-check.out
+grep 'Errors parsing' /tmp/php-check.out && exit 1 || exit 0
+
