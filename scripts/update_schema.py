a5945f387bd001d384fc6f524869b733e92b44f8
Roberto Polli <roberto.polli@par-tec.it>
2021-05-06 20:12:19 +0200

    Fix: #70. Structured implementation fields.

--- scripts/update_schema.py
+++ scripts/update_schema.py
@@ -1,7 +1,10 @@
 # This script is used for temporary schema changes and will not last long
+from re import findall
+from urllib.parse import urlparse
 import sys
 from pathlib import Path
 
+import html2markdown
 import pandas as pd
 from yaml import safe_dump, safe_load
 
@@ -10,6 +13,47 @@ def as_list(risk):
     if isinstance(risk, str):
         return [risk]
     return risk
+
+
+def i_to_dict(i: str):
+        i = html2markdown.convert(i)
+        ret = {"name": i, "tags": []}
+        if ghrepo := findall("https://[a-z/0-9A-Z._-]+", i):
+            repo = urlparse(ghrepo[0]).path.strip("/")
+            ret["url"] = ghrepo[0]
+        if name := findall(r"^\[([^\]]+)\]", i):
+            ret["name"] = name[0]
+        if len(i) > 100:
+            ret["description"] = i
+            ret["name"] = i[:20]
+        ret["name"] = ret["name"].strip()
+        return ret
+
+
+def format_implementation(fpath="data/dimensions.yaml"):
+    # Use the ruamel.yaml to preserve anchors.
+    import ruamel.yaml
+
+    y = ruamel.yaml.YAML(typ="rt")
+
+    dimensions = y.load(Path(fpath).read_text())
+
+    # for dimension, v in dimensions.items():
+    #     if dimension.startswith("_"): continue
+    for subdimension, activity_d in dimensions.items():
+        if subdimension.startswith("_"):
+            continue
+        for activity, data in activity_d.items():
+            if "risk" in data:
+                if isinstance(data["risk"], str):
+                    data["risk"] = [data["risk"]]
+            if v := data.get("implementation"):
+                if isinstance(v, str):
+                    raise ValueError(v)
+                data["implementation"] = [i_to_dict(i) for i in v]
+
+    y.dump(dimensions, stream=Path(fpath))
+    return dimensions
 
 
 def format_references(fpath="data/dimensions.yaml"):
@@ -54,12 +98,14 @@ def format_references(fpath="data/dimensions.yaml"):
     return dimensions
 
 
-def test_format():
+def test_references():
     format_references("data-new/BuildAndDeployment/Sub-Dimensions.yaml")
 
+def test_implementation():
+    format_implementation("data-new/BuildAndDeployment/Sub-Dimensions.yaml")
 
 if __name__ == "__main__":
-    format_references(sys.argv[1])
+    format_implementation(sys.argv[1])
 
 if __name__ == "main__":
 
