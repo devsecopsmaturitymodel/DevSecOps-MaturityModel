c03c7b18d346f337d2326478a2982c85fc925358
Timo Pagel <github@timo-pagel.de>
2021-07-05 16:01:17 +0200

    feat: enhance activities

--- detail.php
+++ detail.php
@@ -10,7 +10,7 @@
 
 
 if (array_key_exists("element", $_GET)) {
-  $title = "Details for '" . htmlspecialchars($_GET['element']) . "'";
+    $title = "Details for '" . htmlspecialchars($_GET['element']) . "'";
 }
 include_once "head.php";
 ?>
@@ -30,94 +30,97 @@ $activityName = $_GET['element'] ?? null;
  * @param unknown $subdimension
  * @param unknown $activityName
  * @param unknown $dimensions
- * @param unknown $report       (optional)
+ * @param unknown $report (optional)
  */
-function printDetail($dimension, $subdimension, $activityName, $dimensions, $report = false) {
-  $element = $dimensions[$dimension][$subdimension][$activityName] ?? null;
+function printDetail($dimension, $subdimension, $activityName, $dimensions, $report = false)
+{
+    $element = $dimensions[$dimension][$subdimension][$activityName] ?? null;
 
-  if ($element == null) { //Whitelist approach for security reasons (deny XSS)
-    //echo "Sorry, we could not found the element";
-    return;
-  }
-  if ($report) {
-    $headerWeight = 3;
-  } else {
-    $headerWeight = 1;
-  }
-
-  $pageH1 = "";
-  if (!$report) {
-    $pageH1 .= $dimension;
-
-    if ($dimension != $subdimension) {
-
-      $pageH1 .= " -> $subdimension";
+    if ($element == null) { //Whitelist approach for security reasons (deny XSS)
+        //echo "Sorry, we could not found the element";
+        return;
     }
-    $pageH1 .= ": $activityName";
-  } else {
-    $pageH1 .= "$activityName";
-  }
-
-  echo "<h$headerWeight>$pageH1</h$headerWeight>";
-  echo build_table_tooltip($element, $headerWeight + 1);
-  echo "<hr/>";
-
-  if (array_key_exists("assessment", $element)) {
-      $Parsedown = new Parsedown();
-      echo "<h" . $headerWeight + 1 . ">Assessment</h". $headerWeight + 1 . ">";
-      echo $Parsedown->text($element["assessment"]);
-  }
-  if (array_key_exists("dependsOn", $element) || array_key_exists("implementation", $element) || array_key_exists("comment", $element) || array_key_exists("meta", $element)) {
-    echo "<h" . ($headerWeight + 1) . ">Additional Information</h" . ($headerWeight + 1) . ">";
-    if (array_key_exists("dependsOn", $element)) {
-      $dependsOn = $element['dependsOn'];
-      $dependencies = implode(", ", $dependsOn);
-      echo "<div><b>Dependencies:</b> $dependencies</div>";
-    }
-    echo getElementContentAndCheckExistence($element, "meta");
-  }
-
-
-  if (array_key_exists("md-description", $element) && !empty($element['md-description'])) {
-    echo "<div style=\"background: #F5F5F5\">";
-    $Parsedown = new Parsedown();
-    echo $Parsedown->text($element['md-description']);
-    echo "</div>";
-  }
-  if (array_key_exists("implementation", $element) && !empty($element['implementation'])) {
-    $implementation = $element['implementation'];
-    echo "<div><b>Implementation hints:</b> ";
-    if (is_array($implementation)) {
-      echo "<ul>";
-      foreach ($implementation as $implementationElement) {
-        echo "<li>";
-        if (is_array($implementationElement)) {
-          echo $implementationElement['name'] . ', ';
-          if (!empty($implementationElement['url'])) {
-            echo '<a href="' . $implementationElement['url'] . '\">Link</a>';
-          }
-          echo ", Tags: ";
-          foreach ($implementationElement['tags'] as $tag) {
-            echo $tag . " ";
-          }
-        } else {
-          echo "<li>$implementationElement</li>";
-        }
-        echo "</li>";
-      }
-      echo "</ul>";
+    if ($report) {
+        $headerWeight = 3;
     } else {
-      echo $implementation;
+        $headerWeight = 1;
     }
-    echo "</div>";
-  }
 
-  if (array_key_exists("comment", $element) && !empty($element['comment'])) {
-    $comment = $element['comment'];
-    echo "<div><b>Comments:</b> $comment</div>";
-  }
+    $pageH1 = "";
+    if (!$report) {
+        $pageH1 .= $dimension;
 
-  printReferences($element);
+        if ($dimension != $subdimension) {
+
+            $pageH1 .= " -> $subdimension";
+        }
+        $pageH1 .= ": $activityName";
+    } else {
+        $pageH1 .= "$activityName";
+    }
+
+    echo "<h$headerWeight>$pageH1</h$headerWeight>";
+    echo build_table_tooltip($element, $headerWeight + 1);
+    echo "<hr/>";
+
+    if (array_key_exists("assessment", $element)) {
+        $Parsedown = new Parsedown();
+        echo "<h" . $headerWeight + 1 . ">Assessment</h" . $headerWeight + 1 . ">";
+        echo $Parsedown->text($element["assessment"]);
+    }
+    if (array_key_exists("dependsOn", $element) || array_key_exists("implementation", $element) || array_key_exists("comment", $element) || array_key_exists("meta", $element)) {
+        echo "<h" . ($headerWeight + 1) . ">Additional Information</h" . ($headerWeight + 1) . ">";
+        if (array_key_exists("dependsOn", $element)) {
+            $dependsOn = $element['dependsOn'];
+            $dependencies = implode(", ", $dependsOn);
+            echo "<div><b>Dependencies:</b> $dependencies</div>";
+        }
+        echo getElementContentAndCheckExistence($element, "meta");
+    }
+
+
+    if (array_key_exists("md-description", $element) && !empty($element['md-description'])) {
+        $Parsedown = new Parsedown();
+        echo $Parsedown->text($element['md-description']);
+    }
+    if (array_key_exists("implementation", $element) && !empty($element['implementation'])) {
+        $implementation = $element['implementation'];
+        echo "<div><h" . ($headerWeight + 2) . ">Implementation hints</h" . ($headerWeight + 2) . ">";
+        if (is_array($implementation)) {
+            echo "<ul>";
+            foreach ($implementation as $implementationElement) {
+                echo "<li>";
+                if (is_array($implementationElement)) {
+                    echo $implementationElement['name'] . ', ';
+                    if (!empty($implementationElement['url'])) {
+                        echo '<a href="' . $implementationElement['url'] . '\">Link</a>';
+                    }
+                    echo ", Tags: ";
+                    foreach ($implementationElement['tags'] as $tag) {
+                        echo $tag . " ";
+                    }
+                } else {
+                    echo "<li>$implementationElement</li>";
+                }
+                echo "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo $implementation;
+        }
+        echo "</div>";
+    }
+    if (array_key_exists("comment", $element) && !empty($element['comment'])) {
+        $comment = $element['comment'];
+        echo "<div><b>Comments:</b> $comment</div>";
+    }
+
+    printReferences($element);
+    if (array_key_exists("credits", $element) && !empty($element['credits'])) {
+        echo "<h" . ($headerWeight + 2) . ">Credits</h" . ($headerWeight + 2) . ">";
+        $Parsedown = new Parsedown();
+        echo "This activity is inspired/copied by/from " . $Parsedown->text($element['credits']);
+    }
 }
 
 
@@ -125,21 +128,22 @@ function printDetail($dimension, $subdimension, $activityName, $dimensions, $rep
  *
  * @param unknown $element
  */
-function printReferences($element) {
-  if (!array_key_exists("references", $element)) {
-    return;
-  }
-  $actionLabels = readYaml("data/strings.yml#/actionLabels");
+function printReferences($element)
+{
+    if (!array_key_exists("references", $element)) {
+        return;
+    }
+    $actionLabels = readYaml("data/strings.yml#/actionLabels");
 
-  $references = $element['references'];
-  foreach ($references as $r => $values) {
-    // if it's not an array, array-ze it. Remove after fixing all yamls.
-    $values = is_array($values) ? $values : array($values);
+    $references = $element['references'];
+    foreach ($references as $r => $values) {
+        // if it's not an array, array-ze it. Remove after fixing all yamls.
+        $values = is_array($values) ? $values : array($values);
 
-    $label = getReferenceLabel($r);
-    echo "<div><h3>$label</h3></div>";
-    echo "<ul><li>" . implode("</li><li>", $values) . "</li></ul>";
-  }
+        $label = getReferenceLabel($r);
+        echo "<div><h3>$label</h3></div>";
+        echo "<ul><li>" . implode("</li><li>", $values) . "</li></ul>";
+    }
 
 
 }
