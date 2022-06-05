e0f9a48590f55a7882bc0cc13748ab87843e078c
Aryan <aryangiroud@gmail.com>
2022-06-05 22:19:55 +0530

    Initial implementation of static table

--- src/app/component/matrix/matrix.component.ts
+++ src/app/component/matrix/matrix.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  Dimension: string;
+  SubDimension: string;
+  Level1: string;
+  Level2: string;
+  Level3: string;
+  Level4: string;
+}
+
+const MATRIX_DATA: PeriodicElement[] = [
+  {Dimension: 'Build and Deployment', SubDimension: 'Build', Level1:'Defined build process',Level2:'Pinning of artifacts',Level3:'Signing of artifacts',Level4:''},
+  {Dimension: 'Build and Deployment', SubDimension: 'Deployment', Level1:'Defined deployment process',Level2:'Defined decommissioning process',Level3:'Rolling update on deployment',Level4:'Blue/Green Deployment'},
+];
+
 @Component({
   selector: 'app-matrix',
   templateUrl: './matrix.component.html',
@@ -12,4 +26,13 @@ export class MatrixComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  displayedColumns: string[] = [
+    'Dimension',
+    'SubDimension',
+    'Level1',
+    'Level2',
+    'Level3',
+    'Level4'];
+
+  dataSource = MATRIX_DATA;
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import {MatTableModule} from '@angular/material/table'; 
 
 const MaterialComponents= [
   MatSidenavModule,
@@ -11,6 +12,7 @@ const MaterialComponents= [
   MatIconModule,
   MatListModule,
   MatDividerModule,
+  MatTableModule
 ];
 
 @NgModule({
