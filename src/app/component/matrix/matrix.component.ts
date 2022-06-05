7ff54107063dd23a1579ea4d93b7873dbc90ac46
Aryan <aryangiroud@gmail.com>
2022-06-06 00:24:57 +0530

    CSS changes to matrix table + list view for level columns

--- src/app/component/matrix/matrix.component.ts
+++ src/app/component/matrix/matrix.component.ts
@@ -3,15 +3,15 @@ import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
   Dimension: string;
   SubDimension: string;
-  Level1: string;
-  Level2: string;
-  Level3: string;
-  Level4: string;
+  Level1: string[];
+  Level2: string[];
+  Level3: string[];
+  Level4: string[];
 }
 
 const MATRIX_DATA: PeriodicElement[] = [
-  {Dimension: 'Build and Deployment', SubDimension: 'Build', Level1:'Defined build process',Level2:'Pinning of artifacts',Level3:'Signing of artifacts',Level4:''},
-  {Dimension: 'Build and Deployment', SubDimension: 'Deployment', Level1:'Defined deployment process',Level2:'Defined decommissioning process',Level3:'Rolling update on deployment',Level4:'Blue/Green Deployment'},
+  {Dimension: 'Build and Deployment', SubDimension: 'Build', Level1: ['Defined build process','ES'], Level2:['Pinning of artifacts'],Level3:['Signing of artifacts'],Level4:[]},
+  {Dimension: 'Build and Deployment', SubDimension: 'Deployment', Level1: ['Defined deployment process','EN'], Level2:['Defined decommissioning process'],Level3:['Rolling update on deployment'],Level4:['Blue/Green Deployment']},
 ];
 
 @Component({
