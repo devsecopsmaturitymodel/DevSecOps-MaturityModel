5304810535d76893a64fdba99b569643027f3483
Aryan <aryangiroud@gmail.com>
2022-06-05 21:15:40 +0530

    CSS changes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav'; 
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list'; 
+import {MatListModule} from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 const MaterialComponents= [
   MatSidenavModule,
   MatButtonModule,
   MatIconModule,
   MatListModule,
+  MatDividerModule,
 ];
 
 @NgModule({
