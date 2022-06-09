1028fa1435cc1d7ada5075536da17b27165a92f5
Aryan <aryangiroud@gmail.com>
2022-06-09 21:31:15 +0530

    Added chips that allow for personalized reconstruction of table rows

--- src/app/component/matrix/matrix.component.ts
+++ src/app/component/matrix/matrix.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef, ViewChild } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface MatrixElement {
   Dimension: string;
@@ -22,6 +28,12 @@ const MATRIX_DATA: MatrixElement[] = [
   Level2:['Defined decommissioning process'],
   Level3:['Rolling update on deployment'],
   Level4:['Temp']},
+  {Dimension: 'Build and Deployment', 
+  SubDimension: 'Depl', 
+  Level1: ['Defined deployment process','EN'], 
+  Level2:['Defined decommissioning process'],
+  Level3:['Rolling update on deployment'],
+  Level4:['Temp']},
 ];
 
 @Component({
@@ -41,25 +53,36 @@ export class MatrixComponent implements OnInit {
     'SubDimension',
     ];
   
-  constructor() { }
+  allRows:string[]=[];
+  dataSource:any= new MatTableDataSource<MatrixElement>(MATRIX_DATA);
+  rows: string[] = [];
+  
+  constructor() {
+    this.filteredRows = this.rowCtrl.valueChanges.pipe(
+      startWith(null),
+      map((row: string | null) => (row ? this._filter(row) : this.autoCompeteRows.slice())),
+    );
+   }
 
-  ngOnInit(): void {
+   // function to initialize if level columns exists
+   ngOnInit(): void {
+    this.dataSource.data = JSON.parse(JSON.stringify(MATRIX_DATA)); ;
     let i =0;
     while((i<MATRIX_DATA.length) && (this.iflvl1exists==false || this.iflvl2exists==false || this.iflvl3exists==false || this.iflvl4exists==false)){
       if((this.iflvl1exists==false) && MATRIX_DATA[i].Level1.length!=0){
-        console.log(MATRIX_DATA[i].Level1);
+        //console.log(MATRIX_DATA[i].Level1);
         this.iflvl1exists=true
       }
       if((this.iflvl2exists==false) && MATRIX_DATA[i].Level2.length!=0){
-        console.log(MATRIX_DATA[i].Level2);
+        //console.log(MATRIX_DATA[i].Level2);
         this.iflvl2exists=true
       }
       if((this.iflvl3exists==false) && MATRIX_DATA[i].Level3.length!=0){
-        console.log(MATRIX_DATA[i].Level3);
+        //console.log(MATRIX_DATA[i].Level3);
         this.iflvl3exists=true
       }
       if((this.iflvl4exists==false) && MATRIX_DATA[i].Level4.length!=0){
-        console.log(MATRIX_DATA[i].Level4);
+        //console.log(MATRIX_DATA[i].Level4);
         this.iflvl4exists=true
       }
       i++;
@@ -76,9 +99,67 @@ export class MatrixComponent implements OnInit {
     if(this.iflvl4exists==true){
       this.displayedColumns.push('Level4');
     };
+    
+    // creates initial row list consisting of all rows 
+    i=0;
+    while(i<MATRIX_DATA.length){
+      if(!this.allRows.includes(MATRIX_DATA[i].SubDimension)){
+        this.allRows.push(MATRIX_DATA[i].SubDimension);
+        this.rows.push(MATRIX_DATA[i].SubDimension);
+      }
+      i++;
+    }
+    console.log(this.allRows)
+
+  }
+
+
+
+  //chips
   
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  rowCtrl = new FormControl('');
+  filteredRows: Observable<string[]>;
+  
+  autoCompeteRows: string[] = [];
+
+  @ViewChild('rowInput') rowInput!: ElementRef<HTMLInputElement>;
+   
+  //Remove chips 
+  remove(row: string): void {
+    let index = this.rows.indexOf(row);
+    //console.log(this.allRows);
+    if (index >= 0) {
+      this.rows.splice(index, 1);
+    }
+    this.autoCompeteRows.push(row);
+    this.dataSource.data.splice(index,1);
+    this.dataSource._data.next(this.dataSource.data)
+  }
+
+  //Add chips
+  selected(event: MatAutocompleteSelectedEvent): void {
+    let autoIndex = this.autoCompeteRows.indexOf(event.option.viewValue);
+    this.autoCompeteRows.splice(autoIndex,1);
+    this.rows.push(event.option.viewValue);
+    this.rowInput.nativeElement.value = '';
+    this.rowCtrl.setValue(null);
+    //console.log(this.allRows,event.option.viewValue);
+    let dataIndex=this.allRows.indexOf(event.option.viewValue);
+    this.dataSource.data.push(MATRIX_DATA[dataIndex]);
+    //console.log(dataIndex);
+    //console.log(MATRIX_DATA)
+    //console.log(this.dataSource.data);
+    this.dataSource._data.next(this.dataSource.data);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.autoCompeteRows.filter(row => row.toLowerCase().includes(filterValue));
   }
 
   
-  dataSource = MATRIX_DATA;
+  
+
 }
