172d52261da3aa6b2699ca600e3dd16bec4f2b19
Aryan <aryangiroud@gmail.com>
2022-10-29 20:46:10 +0530

    Added more tests

--- src/app/component/top-header/top-header.component.spec.ts
+++ src/app/component/top-header/top-header.component.spec.ts
@@ -21,4 +21,19 @@ describe('TopHeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('check if header is being generated', () => {
+    const HTMLElement: HTMLElement = fixture.nativeElement;
+    const heading = HTMLElement.querySelector('h1')!;
+    expect(heading.textContent).toEqual('Default');
+  });
+
+  it('check if header is being changed', () => {
+    const changedTextElement = 'changed';
+    component.section = changedTextElement;
+    fixture.detectChanges();
+    const HTMLElement: HTMLElement = fixture.nativeElement;
+    const heading = HTMLElement.querySelector('h1')!;
+    expect(heading.textContent).toEqual('changed');
+  });
 });
