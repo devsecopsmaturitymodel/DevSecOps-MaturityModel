0f0dcefd8c73bde719d6f8aeed8b92a7fd0473e7
Prakarsh Gupta <prakarshgupta02051@gmail.com>
2023-08-14 13:34:03 +0530

    Fixed Unit Test: added matChip in declaration of material module

--- src/app/component/circular-heatmap/circular-heatmap.component.spec.ts
+++ src/app/component/circular-heatmap/circular-heatmap.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ymlService } from 'src/app/service/yaml-parser/yaml-parser.service';
 import { CircularHeatmapComponent } from './circular-heatmap.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatChip } from '@angular/material/chips';
 
 describe('CircularHeatmapComponent', () => {
   let component: CircularHeatmapComponent;
@@ -13,6 +14,11 @@ describe('CircularHeatmapComponent', () => {
       providers: [ymlService, HttpClient, HttpHandler],
       imports: [RouterTestingModule],
       declarations: [CircularHeatmapComponent],
+    }).compileComponents();
+  });
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [MatChip],
     }).compileComponents();
   });
 
