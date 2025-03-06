20266da01a47b2bdf453828a69d80de29bb85d91
aguero-tech <developer@aguero.tech>
2025-03-06 02:45:18 -0600

    added the dark theme, messy but works.  need to add notes to the custom-theme.scss file for references and changes from original.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']  // This is for component-specific styles.
 })
 export class AppComponent {
   title = 'DSOMM';
+  isNightMode = false;
+
+  toggleTheme() {
+    this.isNightMode = !this.isNightMode;
+    if (this.isNightMode) {
+      document.body.classList.add('night-mode');
+    } else {
+      document.body.classList.remove('night-mode');
+    }
+  }
 }
