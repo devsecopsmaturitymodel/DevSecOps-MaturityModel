1a89e3a9f1908f3e434ee8cb37e6a58e4d83981a
Vegard Bakke <vegard.bakke@owasp.org>
2025-10-21 21:55:14 +0200

    Activity: Display Tags and Implemented by

--- src/app/component/activity-description/activity-description.component.ts
+++ src/app/component/activity-description/activity-description.component.ts
@@ -13,6 +13,7 @@ import {
 import { MatAccordion } from '@angular/material/expansion';
 import { Activity } from '../../model/activity-store';
 import { LoaderService } from '../../service/loader/data-loader.service';
+import { TeamName, ProgressTitle } from '../../model/types';
 
 @Component({
   selector: 'app-activity-description',
@@ -36,6 +37,9 @@ export class ActivityDescriptionComponent implements OnInit, OnChanges {
   ISO22Version: string = 'ISO 27001:2022';
   openCREVersion: string = 'OpenCRE';
   isNarrowScreen: boolean = false;
+  teamsImplemented: Map<TeamName, ProgressTitle> = new Map();
+  teamsByProgressTitle: Map<ProgressTitle, TeamName[]> = new Map();
+  progressTitlesWithTeams: ProgressTitle[] = [];
 
   @ViewChildren(MatAccordion) accordion!: QueryList<MatAccordion>;
 
@@ -75,6 +79,53 @@ export class ActivityDescriptionComponent implements OnInit, OnChanges {
       this.ResourceLabel = dataStore.getMetaString('labels', activity.difficultyOfImplementation.resources - 1);
       this.UsefulnessLabel = dataStore.getMetaString('labels', activity.usefulness - 1);
       /* eslint-enable */
+      
+      // Get teams that have implemented this activity
+      this.updateTeamsImplemented();
+    }
+  }
+
+  updateTeamsImplemented() {
+    this.teamsImplemented.clear();
+    this.teamsByProgressTitle.clear();
+    this.progressTitlesWithTeams = [];
+    
+    const dataStore = this.loader.datastore;
+    if (!dataStore || !dataStore.progressStore || !dataStore.meta || !this.currentActivity.uuid) {
+      return;
+    }
+
+    const teams = dataStore.meta.teams;
+    const progressStore = dataStore.progressStore;
+    const activityUuid = this.currentActivity.uuid;
+
+    // Get all progress titles (excluding the first one which is "Not started")
+    const inProgressTitles = progressStore.getInProgressTitles();
+    const completedTitle = progressStore.getCompletedProgressTitle();
+    const allProgressTitles = [...inProgressTitles, completedTitle];
+
+    // Check each team to see if they have started or completed this activity
+    for (const teamName of teams) {
+      const progressTitle = progressStore.getTeamProgressTitle(activityUuid, teamName);
+      const progressValue = progressStore.getTeamActivityProgressValue(activityUuid, teamName);
+      
+      // Only include teams that have made progress (value > 0)
+      if (progressValue > 0) {
+        this.teamsImplemented.set(teamName, progressTitle);
+        
+        // Group teams by progress title
+        if (!this.teamsByProgressTitle.has(progressTitle)) {
+          this.teamsByProgressTitle.set(progressTitle, []);
+        }
+        this.teamsByProgressTitle.get(progressTitle)!.push(teamName);
+      }
+    }
+
+    // Create ordered list of progress titles that have teams (skip "Not started")
+    for (const progressTitle of allProgressTitles) {
+      if (this.teamsByProgressTitle.has(progressTitle)) {
+        this.progressTitlesWithTeams.push(progressTitle);
+      }
     }
   }
 
