ed0349d34ed46cf1e6625af98a1d31bb22783f2b
keshav lath <keshav.lath3232788@gmail.com>
2026-03-30 22:48:37 +0530

    refactor mapping page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatMenuModule } from '@angular/material/menu';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -71,6 +71,7 @@ import { ColResizeDirective } from './directive/col-resize.directive';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    MatPaginatorModule,
     MatDialogModule,
     ReactiveFormsModule,
     MatToolbarModule,
