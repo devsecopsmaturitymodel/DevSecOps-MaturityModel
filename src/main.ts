b057977869361b19b6572ea046e630d904a75dc7
Abdullah D. <48760103+abdebek@users.noreply.github.com>
2026-03-31 01:45:50 +0300

    feat: migrate workspace to Angular 21

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ document.body.classList.remove('light-theme', 'dark-theme');
 document.body.classList.add(`${savedTheme}-theme`);
 console.log('[main.ts] Theme set to:', savedTheme); //
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -14,5 +14,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()] })
   .catch(err => console.error(err));
