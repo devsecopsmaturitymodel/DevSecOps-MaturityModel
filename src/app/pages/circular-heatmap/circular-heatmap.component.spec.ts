e518774305b029efdddede321aaf8046391d93f4
Abdullah D. <48760103+abdebek@users.noreply.github.com>
2026-03-31 02:04:09 +0300

    refactor: remove zone.js for zoneless Angular 21

--- src/app/pages/circular-heatmap/circular-heatmap.component.spec.ts
+++ src/app/pages/circular-heatmap/circular-heatmap.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { LoaderService } from 'src/app/service/loader/data-loader.service';
 import { CircularHeatmapComponent } from './circular-heatmap.component';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -19,7 +20,12 @@ describe('CircularHeatmapComponent', () => {
         LoaderService,
         HttpClient,
         HttpHandler,
-        { provide: ModalMessageComponent, useValue: {} },
+        {
+          provide: ModalMessageComponent,
+          useValue: {
+            openDialog: vi.fn(() => ({ afterClosed: () => of('Cancel') })),
+          },
+        },
       ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
